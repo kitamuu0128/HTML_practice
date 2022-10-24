5a585297ebfa67a98584a3570d745ec07815558f
kitamuu0128 <kitamuu198@gmail.com>
2022-10-24 22:13:27 +0900

    [add]Webアプリケーションを作ってみよう-2の課題を追加しました

--- omikuji.js
+++ omikuji.js
@@ -2,6 +2,11 @@ let username;
 let userresult;
 username = prompt("お名前を教えて下さい。");
 document.getElementById("name").innerHTML = username;
+
+if(username==""){
+  document.getElementById("name").innerHTML="名無し";
+}
+
 let rand = Math.floor( Math.random() * 5) ;
 if (rand == 0) {
     userresult = "大吉";
@@ -18,4 +23,5 @@ if (rand == 0) {
   if (rand == 4) {
     userresult = "凶";
   }
+
   document.getElementById("result").innerHTML = userresult;
